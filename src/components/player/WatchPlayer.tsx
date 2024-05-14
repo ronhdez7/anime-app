c01becb3e1025138ecc1e34f0be735be4c1ecab0
ronald hernandez <ronaldhp2007@gmail.com>
2024-05-14 01:20:30 -0400

    updated libs but running fails

--- src/components/player/WatchPlayer.tsx
+++ src/components/player/WatchPlayer.tsx
@@ -1,6 +1,12 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { MALID } from "@/types/jikan";
+import { useAnime } from "@/queries/use-anime";
+import { useFindAnime } from "@/queries/use-find-anime";
+import { useStreamEpisodes } from "@/queries/use-stream-episodes";
+import { useStreamServers } from "@/queries/use-stream-servers";
+import { useStreamSources } from "@/queries/use-stream-sources";
+import { useVideoPlayer } from "expo-video";
+import PlayerWithControls from "./PlayerWithControls";
 
 interface WatchPlayerProps {
   animeId: MALID;
@@ -11,9 +17,20 @@ export default function WatchPlayer({
   animeId,
   episodeNumber,
 }: WatchPlayerProps) {
-  return (
-    <View>
-      <Text>WatchPlayer</Text>
-    </View>
+  const anime = useAnime(animeId);
+  const streamAnime = useFindAnime(
+    { title: anime.data?.titles.jp, title_en: anime.data?.titles.en },
+    !!anime.data
   );
+  const episodes = useStreamEpisodes(streamAnime.data?.url);
+  const servers = useStreamServers(
+    episodes.data?.find((i) => i.number === episodeNumber)?.url ?? undefined
+  );
+  const sources = useStreamSources(servers.data?.at(0)?.playerUrl);
+
+  const player = useVideoPlayer({
+    uri: sources.data?.sources.at(0)?.url ?? "",
+  });
+
+  return <PlayerWithControls player={player} />;
 }
